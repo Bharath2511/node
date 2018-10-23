9b71c1d53c737b767958cf8999ece9be9a1abbfc
V S Bharath <bharathcse5t6@gmail.com>
2018-10-23 17:57:01 +0530

    fs module

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const path = require('path')
 const os = require('os')
+const fs = require('fs')
 
 var pathObj = path.parse(__filename)
 console.log(pathObj)
  console.log(os.totalmem(),os.freemem())
 
+console.log(fs.readdirSync('./'))
+console.log(fs.readdir('./',(err,files)=>{
+    if (err) console.log(err)
+    else console.log(files)
+
+}))
